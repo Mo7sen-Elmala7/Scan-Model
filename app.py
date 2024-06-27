97f9ef1a4c327ba3c401a568bd3c2950860c831a
Mo7sen-Elmala7 <103606106+Mo7sen-Elmala7@users.noreply.github.com>
2024-06-28 01:47:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -90,5 +90,5 @@ def scan_face():
     return jsonify({"error": "Face not recognized"}), 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
-
+    port = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=port, debug=True)
