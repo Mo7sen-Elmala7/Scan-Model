85a9312f3d28a9d4dbc4fa81a4d6397dd7eae0b6
Mo7sen-Elmala7 <103606106+Mo7sen-Elmala7@users.noreply.github.com>
2024-06-28 00:37:00 +0300

    Update and rename build.sh to install.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install CMake
-pip install cmake
-
-# Install other dependencies
-pip install -r requirements.txt
---
+++ install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pip install ./wheels/dlib-19.22.0-cp39-cp39-win_amd64.whl
